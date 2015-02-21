30df1f23fda71c660c12fd705518d1db2f8df3d7
m <m@m-2.local>
2015-02-21 13:17:55 +1030

    Finish defining basic classes; Bicycle, Shop and Customer

--- bikes.py
+++ bikes.py
@@ -1,4 +1,5 @@
 class Bicycle(object):
+    # Define a bike object that has a model, weight and cost attributes
     def __init__(self, model, weight, cost):
         self.model = model
         self.weight = weight
@@ -6,17 +7,26 @@ class Bicycle(object):
 
 
 class Shop(object):
+    # Define bike shop that has a name, inventory and profit attributes
     def __init__(self, name):
         self.name = name
         self.inventory = []
         self.profit = 0
 
     def sell(self, bicycle):
+        # Sell method for bike shop with profit margin on each bike sale
         self.inventory.remove(bicycle)
         margin = (bicycle.cost * 1.2) - bicycle.cost
         self.profit += margin
         return margin
 
+class Customer(object):
+    # Define a customer who has a name and who has a fund of dollars
+    # to buy a bike. If no value for fund is supplied, set to $500
+    def __init__(self, name, fund):
+        self.name = name
+        self.ride = None # Customers initial ride is none
+        self.fund = fund if fund is not None else 500
 
 
 
@@ -32,3 +42,7 @@ shop1.inventory.append(bike1)
 print shop1.inventory
 shop1.sell(bike1)
 print shop1.profit
+jake = Customer("Jake", 900)
+print jake.name
+print jake.ride
+print jake.fund
